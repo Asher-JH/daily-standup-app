037afdce9336207630d179cfca915ad9f4bef698
Asher <asherchan2002@hotmail.com>
2021-12-20 02:57:59 +0800

    Chakra UI theme config & fonts

---
+++ client/src/config/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: "Manrope, sans-serif",
+    heading: "Manrope, sans-serif",
+    mono: "Manrope, sans-serif",
+  },
+});
+
+export default theme;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { ColorModeScript } from "@chakra-ui/react";
+import "@fontsource/manrope";
+
+import { App } from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <ColorModeScript />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
